4ed441bbbd19c52eab1e4fc4f430c240e78e2db6
xk <1548051137@qq.com>
2020-01-05 19:27:13 +0800

    first

---
+++ src/main/java/com/example/demo/DemoAutoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoAutoApplication.class, args);
+    }
+
+}
